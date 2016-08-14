e1fd2975e492a5fe08d365beaf60058d3f1e1fe5
lshrinivas <lakshmikant.shrinivas@nutonian.com>
2016-08-14 17:03:09 -0400

    More style tweaks

--- page-home.php
+++ page-home.php
@@ -16,10 +16,46 @@ get_header(); ?>
 
     <?php
         $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+        $carousel_image = get_attached_media( 'image' );
     ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            <div class="container-fluid home_container" style="background-image:url(<?php echo $feat_image; ?>)">
+            <div class="container-fluid home_container">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <?php
+                            $activeClass = "active";
+                            foreach($carousel_image as $img) {
+                        ?>
+                                <div class="item <?php echo $activeClass; ?>">
+                                    <img src="<?php echo wp_get_attachment_image_src( $img->ID, 'full' )[0]; ?>" alt="Carousel image">
+                                    <div class="carousel-caption">
+                                    </div>
+                                </div>
+                        <?php
+                                $activeClass = "";
+                            }
+                        ?>
+                    </div>
+
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                </div>
                 <div class="home_logo-container">
                     <img src="<?php header_image(); ?>" class="logo-image" alt="TEAM Logo">
                 </div>

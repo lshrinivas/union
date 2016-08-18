5de7bbc445464262b8655323cbf8e2efe03be70d
lshrinivas <lakshmikant.shrinivas@nutonian.com>
2016-08-17 23:30:02 -0400

    Style tweaks

--- footer.php
+++ footer.php
@@ -14,7 +14,16 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="container site-footer" role="contentinfo">
-		If you or someone you know needs immediate help, please call the Crisis Hotline: 800.273.8255 (TALK).
+        <div class="divider"></div>
+	    <div>
+    		If you or someone you know needs immediate help, please call the Crisis Hotline: 800.273.8255 (TALK).
+	    </div>
+	    <div>
+	        <a href="google.com">Contact Us</a>
+	        |
+	        <a href="google.com">Privacy Policy</a>
+	    </div>
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 

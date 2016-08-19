b1a1edc8f6a896a6ab645fbc8665c8f7ad580398
lshrinivas <lakshmikant.shrinivas@nutonian.com>
2016-08-18 23:37:55 -0400

    Update page template

--- page.php
+++ page.php
@@ -12,7 +12,7 @@
  * @package Union
  */
 
-get_header(); ?>
+/*get_header();*/ ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -23,9 +23,9 @@ get_header(); ?>
 				get_template_part( 'template-parts/content', 'page' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+//				if ( comments_open() || get_comments_number() ) :
+//					comments_template();
+//				endif;
 
 			endwhile; // End of the loop.
 			?>
@@ -34,5 +34,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+/*get_sidebar();*/
 get_footer();

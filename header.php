efb3ee575b0c35087c08f7c92a8314e9e51e01cb
lshrinivas <lakshmikant.shrinivas@nutonian.com>
2016-08-20 18:36:19 -0400

    some read more JS

--- header.php
+++ header.php
@@ -29,9 +29,9 @@
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-<!-- site javascript -->
-<script src="/js/site.js"></script>
-
+<?php
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/site.js', array (), 1.1, true);
+?>
 <?php wp_head(); ?>
 </head>
 

8fc56c448ade577c7668c2d5203b3ac26b908d47
lshrinivas <lakshmikant.shrinivas@nutonian.com>
2016-08-20 19:29:09 -0400

    Read more and read less fixes

--- js/site.js
+++ js/site.js
@@ -1,7 +1,16 @@
 $(function() {
-    $('a.read_more').click(function(event){ /* find all a.read_more elements and bind the following code to them */
+    /* find all a.read_more elements and bind the following code to them */
+    $('a.read_more').click(function(event){
 
         event.preventDefault(); /* prevent the a from changing the url */
         $(this).parents('.item').find('.more_text').show(); /* show the .more_text span */
+        $(this).hide();
+    });
+    /* find all a.read_less elements and bind the following code to them */
+    $('a.read_less').click(function(event){
+
+        event.preventDefault(); /* prevent the a from changing the url */
+        $(this).parents('.item').find('.more_text').hide(); /* hide the .more_text span */
+        $(this).parents('.item').find('.read_more').show();
     });
 });

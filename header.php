50eb8c35522cc9ebb94769fa6e871c2a16e9bc7f
lshrinivas <lakshmikant.shrinivas@nutonian.com>
2016-08-20 17:27:25 -0400

    Internal page header

--- header.php
+++ header.php
@@ -39,19 +39,57 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'union' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-		    <?php if ( !is_front_page() ) : ?>
-                <div class="logo_container">
-                    <?php if ( get_header_image() ) : ?>
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                            <img src="<?php header_image(); ?>" class="logo-image" alt="TEAM Logo">
-                        </a>
-                    <?php endif; /*End header image check.*/ ?>
+        <?php if ( !is_front_page() && get_header_image() ) : ?>
+            <div class="container">
+                <div class="media">
+                <div class="logo_container media-left">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                        <img src="<?php header_image(); ?>" class="logo-image" alt="TEAM Logo">
+                    </a>
                 </div>
-            <?php endif; /*End front page check.*/ ?>
+                <div class="nav-container media-body">
+                    <div class="row nav-right">
+                        <div class="social_media">
+                            <?php echo DISPLAY_ULTIMATE_PLUS(); ?>
+                        </div>
+                        <div>
+                            <a href="#">
+                                <button class="btn btn-success donate_button">
+                                    Donate
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row btn-success">
+                        <nav id="site-navigation" class="navbar navbar-default navbar-internal" role="navigation">
+                            <div class="navbar-header navbar-left">
+                                <button type="button" class="navbar-toggle pull-left collapsed" data-toggle="collapse" data-target="#main_nav" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                            </div>
+                            <div class="collapse navbar-collapse" id="main_nav">
+                                <?php wp_nav_menu( array(
+                                    'theme_location' => 'primary',
+                                    'menu_id' => 'primary-menu',
+                                    'menu_class' => 'nav navbar-nav',
+                                     'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+                                     'walker' => new wp_bootstrap_navwalker()
+                                 ) ); ?>
+                            </div>
+                        </nav><!-- #site-navigation -->
+                    </div>
+                </div>
+
+                </div>
+
+    		</div><!-- .container -->
+        <?php else : ?>
             <div class="container">
                 <div class="row">
-                    <div class="col-xs-7">
+                    <div class="col-xs-5 col-sm-7">
                         <nav id="site-navigation" class="navbar navbar-default" role="navigation">
                             <div class="navbar-header navbar-left">
                                 <button type="button" class="navbar-toggle pull-left collapsed" data-toggle="collapse" data-target="#main_nav" aria-expanded="false">
@@ -72,7 +110,7 @@
                             </div>
                         </nav><!-- #site-navigation -->
                     </div>
-                    <div class="col-xs-5 nav-right">
+                    <div class="col-xs-7 col-sm-5 nav-right">
                         <div class="social_media">
                             <?php echo DISPLAY_ULTIMATE_PLUS(); ?>
                         </div>
@@ -86,9 +124,7 @@
                     </div>
                 </div>
             </div>
-		</div><!-- .site-branding -->
-
-
+        <?php endif; /*End front page check.*/ ?>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
--- page.php
+++ page.php
@@ -12,20 +12,15 @@
  * @package Union
  */
 
-/*get_header();*/ ?>
+get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="container" role="main">
 
 			<?php
 			while ( have_posts() ) : the_post();
 
 				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-//				if ( comments_open() || get_comments_number() ) :
-//					comments_template();
-//				endif;
 
 			endwhile; // End of the loop.
 			?>
@@ -34,5 +29,4 @@
 	</div><!-- #primary -->
 
 <?php
-/*get_sidebar();*/
 get_footer();

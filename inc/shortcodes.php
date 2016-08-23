bddb4f3294a7941e4ba7173c3302102965bf3593
lshrinivas <lakshmikant.shrinivas@nutonian.com>
2016-08-22 20:15:51 -0400

    Style tweaks

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -5,12 +5,43 @@
  * @package Union
  */
 
-// [resource_video url="video url"]
+// [resource_video url="video url" title="title" text="description"]
 function resource_video_func( $atts ) {
     $a = shortcode_atts( array(
-        'url' => ''
+        'url' => '',
+        'title' => 'Video',
+        'text' => 'Description'
     ), $atts );
 
-    return '<div class="resource_video">' . "{$a['url']}" . '</div>';
+    $html = <<<EOB
+<div class="thumbnail">
+  <video controls style="width:100%;">
+    <source src="{$a['url']}">
+  </video>
+  <div class="caption">
+    <h3>{$a['title']}</h3>
+    <p>{$a['text']}</p>
+  </div>
+</div>
+EOB;
+
+    return $html;
+}
+add_shortcode( 'resource_video', 'resource_video_func' );
+
+// [tinted_image img_url="image url" color="rgba(...)"]
+function tinted_image_func( $atts ) {
+    $a = shortcode_atts( array(
+        'img_url' => '',
+        'color' => 'rgba(255, 255, 255, 0.3)'
+    ), $atts );
+
+    $html = <<<EOB
+<div style="background-image: url({$a['img_url']}); background-size: cover; background-repeat: no-repeat; background-position: left top;">
+
+</div>
+EOB;
+
+    return $html;
 }
 add_shortcode( 'resource_video', 'resource_video_func' );

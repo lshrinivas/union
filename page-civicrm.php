5d1aff50c0a04d7e2b037c10d95a11452ee26447
lshrinivas <lakshmikant.shrinivas@nutonian.com>
2016-08-27 18:13:19 -0400

    CiviCRM tweaks

--- page-civicrm.php
+++ page-civicrm.php
@@ -16,15 +16,25 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="container" role="main">
+			<div style="background-image: url(http://www.teamasianminds.org/wp-content/uploads/2016/08/join-us.jpg); background-position=center center;" class="tinted_image">
+				<div class="tinted_image-tint" style="background-color: rgba(255,255,255,0.8)"></div>
+				<div class="tinted_image-content">
+                    <div class = "container">
+                        <div class = "row">
+                            <div class="col-sm-8 col-sm-offset-2"  >
+                                <?php
+                                while ( have_posts() ) : the_post();
 
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
+                                    get_template_part( 'template-parts/content', 'page' );
 
 
-			endwhile; // End of the loop.
-			?>
+                                endwhile; // End of the loop.
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+				</div>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->

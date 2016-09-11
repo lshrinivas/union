df96eaf52438589f2523c1264d2c1b6de69cc6f3
lshrinivas <lakshmikant.shrinivas@nutonian.com>
2016-09-11 15:48:44 -0400

    Style tweaks

--- page.php
+++ page.php
@@ -14,19 +14,12 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="container" role="main">
-
-			<?php
+	<div class="container-fluid">
+		<?php
 			while ( have_posts() ) : the_post();
-
 				get_template_part( 'template-parts/content', 'page' );
-
-
 			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
+		?>
 	</div><!-- #primary -->
 
 <?php

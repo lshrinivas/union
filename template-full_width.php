b0912c310fca197ece340ccec9f11be313182814
lshrinivas <lakshmikant@gmail.com>
2017-02-19 21:37:15 -0500

    Fixed header style tweaks

--- template-full_width.php
+++ template-full_width.php
@@ -10,7 +10,7 @@
 
 get_header("fixed_menu"); ?>
 
-	<div id="primary" class="content-area content-fixed">
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
             <div class="container-fluid">
                 <?php
